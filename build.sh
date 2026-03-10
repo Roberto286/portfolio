a039f92e6f68746aed9a6fae67bd60ad80b8f190
Roberto Saliola <roberto.saliola96@gmail.com>
2026-03-10 16:34:07 +0100

    fix build script maybe2

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ main() {
 
   # Build the site
   echo "Building the site"
-  hugo --contentDir ./site --gc --minify --destination ./public --baseURL "https://${VERCEL_PROJECT_PRODUCTION_URL}"
+  cd site && hugo --gc --minify --destination ../public --baseURL "https://${VERCEL_PROJECT_PRODUCTION_URL}"
 
 }
 
